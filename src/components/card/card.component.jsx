5881f69695c6b8d8e7ecb4ba421f88be21bf2e0e
Zhandos Sembayev <sembayev.zhandos@gmail.com>
2022-06-14 16:34:58 +0600

    feat: added pictures to each user

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 
 const Card = (props) => {
-  return <div>{props.monsters.map((monster) => {
-    return <h1 key={monster.id}>{monster.name}</h1>
-  })}</div>;
+  return (
+    <div>
+      {props.monsters.map((monster) => {
+        return (
+          <div key={monster.id}>
+            <img src={`https://robohash.org/${monster.id}?set=set2&size=100x100`}/>
+            <h1 >{monster.name}</h1>
+            <p>{monster.email}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Card;
